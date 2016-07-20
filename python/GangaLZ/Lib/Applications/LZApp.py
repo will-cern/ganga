8cff24ef44381ff405da0689b06999163778640f
Alexander Richards <a.richards@imperial.ac.uk>
2016-07-20 13:55:14 +0100

    Added more stuff

--- python/GangaLZ/Lib/Applications/LZApp.py
+++ python/GangaLZ/Lib/Applications/LZApp.py
@@ -3,18 +3,16 @@
 #
 # $Id: IApplication.py,v 1.1 2008-07-17 16:40:52 moscicki Exp $
 ##########################################################################
+import os
+import logging
+from textwrap import dedent
 
 from Ganga.GPIDev.Adapters.IApplication import IApplication
 from Ganga.GPIDev.Adapters.StandardJobConfig import StandardJobConfig
 from Ganga.GPIDev.Schema import Schema, Version, SimpleItem
-import textwrap
-import os
-import logging
-from Ganga.Utility.Config import getConfig
-from Ganga.Utility.files import expandfilename
+from Ganga.GPIDev.Lib.File.FileBuffer import FileBuffer
 logger = logging.getLogger(__name__)
 
-config = getConfig("Preparable")
 
 class LZApp(IApplication):
 
@@ -27,69 +25,78 @@ class LZApp(IApplication):
         'g4_version':SimpleItem(defvalue='4.9.5.p02', typelist=[str]),
         'libnest_version': SimpleItem(defvalue='2.11.0', typelist=[str]),
         'reduction_version':SimpleItem(defvalue='3.10.0', typelist=[str]),
+        'macro':SimpleItem(defvalue='/home/hep/arichard/git/ganga/macroTemplate.mac', typelist=[str]),
+        'njobs':SimpleItem(defvalue=3, typelist=[int]),
+        'nevents':SimpleItem(defvalue=100000, typelist=[int]),
+        'seed':SimpleItem(defvalue=9000000, typelist=[int]),
     })
     _category = 'applications'
 
-    def __init__(self):
-        super(IApplication, self).__init__()
-
-    def run_script(self):
-        return textwrap.dedent(r"""
-        #!/bin/bash
-        #Prepare some variables based on the inputs
-        MACRO_FILE=$1
-        export SEED=$2 # an integer that will label the output file
-        LUXSIM_DIR=/cvmfs/lz.opensciencegrid.org/LUXSim/release-###LUXSIM_VERSION###/
-        ROOT_DIR=/cvmfs/lz.opensciencegrid.org/ROOT/v5.34.32/slc6_gcc44_x86_64/root/
-        G4_DIR=/cvmfs/lz.opensciencegrid.org/geant4/
-        G4VER=geant###G4_VERSION###
-        LIBNEST_DIR=/cvmfs/lz.opensciencegrid.org/fastNEST/
-        LIBNEST_VERSION=###LIBNEST_VERSION###
-        DATA_STORE_PATH=/lz/data
-        DATA_STORE_SE=UKI-LT2-IC-HEP-disk
-        BIGROOT_STORE_PATH=$DATA_STORE_PATH/LUXSim_$(basename $LUXSIM_DIR)_$G4VER/$(basename $MACRO_FILE _parametric.mac)
-
-        REDUCTION_VERSION=###REDUCTION_VERSION###
-        REDUCTION_LABEL=reduced_v$REDUCTION_VERSION
-        CONVERTER_EXE=/cvmfs/lz.opensciencegrid.org/TDRAnalysis/release-$REDUCTION_VERSION/ReducedAnalysisTree/LZSim2AnalysisTree
-        REDUCED_STORE_PATH=$DATA_STORE_PATH/LUXSim_$(basename $LUXSIM_DIR)_$G4VER/$REDUCTION_LABEL/$(basename $MACRO_FILE _parametric.mac)
-
-
-        export OUTPUT_DIR=$(pwd)
-        #extract the name of the output file from the LUXSim macro
-        OUTPUT_FILE=$(awk '/^\/LUXSim\/io\/outputName/ {print $2}' $1 | tail -1)$2.bin
-
-
-        # move into the LUXSim directory, set G4 env, and run the macro
-        cd $LUXSIM_DIR
-        source $G4_DIR/etc/geant4env.sh $G4VER
-        $LUXSIM_DIR/LUXSimExecutable $OUTPUT_DIR/$MACRO_FILE
-
-        # after macro has run, rootify
-        cd $OUTPUT_DIR
-        source $ROOT_DIR/bin/thisroot.sh
-        $LUXSIM_DIR/tools/LUXRootReader $OUTPUT_FILE
-        OUTPUT_ROOT_FILE=$(basename $OUTPUT_FILE .bin).root
-
-        # reduce and then copy both to our central storage.
-
-        source $LIBNEST_DIR/release-$LIBNEST_VERSION/libNEST/thislibNEST.sh
-        OUTPUT_REDUCED_FILE=${OUTPUT_ROOT_FILE/".root"/"_analysis_tree.root"}
-        $CONVERTER_EXE $OUTPUT_DIR/$OUTPUT_ROOT_FILE $OUTPUT_DIR/$OUTPUT_REDUCED_FILE
-
-        dirac-dms-add-file $BIGROOT_STORE_PATH/$OUTPUT_ROOT_FILE $OUTPUT_DIR/$OUTPUT_ROOT_FILE $DATA_STORE_SE
-        dirac-dms-add-file $REDUCED_STORE_PATH/$OUTPUT_REDUCED_FILE $OUTPUT_DIR/$OUTPUT_REDUCED_FILE $DATA_STORE_SE
-        """).replace('###LUXSIM_VERSION###', self.luxsim_version)\
-            .replace('###G4_VERSION###', self.g4_version)\
-            .replace('###LIBNEST_VERSION###', self.libnest_version)\
-            .replace('###REDUCTION_VERSION###', self.reduction_version)
+    runscript = dedent(r"""    
+    #!/bin/bash
+    #Prepare some variables based on the inputs
+    MACRO_FILE=$1
+    export SEED=$2 # an integer that will label the output file
+    LUXSIM_DIR=/cvmfs/lz.opensciencegrid.org/LUXSim/release-{luxsim_version!s}/
+    ROOT_DIR=/cvmfs/lz.opensciencegrid.org/ROOT/v5.34.32/slc6_gcc44_x86_64/root/
+    G4_DIR=/cvmfs/lz.opensciencegrid.org/geant4/
+    G4VER=geant{g4_version!s}
+    LIBNEST_DIR=/cvmfs/lz.opensciencegrid.org/fastNEST/
+    LIBNEST_VERSION={libnest_version!s}
+    DATA_STORE_PATH=/lz/data
+    DATA_STORE_SE=UKI-LT2-IC-HEP-disk
+    BIGROOT_STORE_PATH=$DATA_STORE_PATH/LUXSim_$(basename $LUXSIM_DIR)_$G4VER/$(basename $MACRO_FILE _parametric.mac)
+    
+    REDUCTION_VERSION={reduction_version!s}
+    REDUCTION_LABEL=reduced_v$REDUCTION_VERSION
+    CONVERTER_EXE=/cvmfs/lz.opensciencegrid.org/TDRAnalysis/release-$REDUCTION_VERSION/ReducedAnalysisTree/LZSim2AnalysisTree
+    REDUCED_STORE_PATH=$DATA_STORE_PATH/LUXSim_$(basename $LUXSIM_DIR)_$G4VER/$REDUCTION_LABEL/$(basename $MACRO_FILE _parametric.mac)
+    
+    
+    export OUTPUT_DIR=$(pwd)
+    #extract the name of the output file from the LUXSim macro
+    OUTPUT_FILE=$(awk '/^\/LUXSim\/io\/outputName/ {{print $2}}' $1 | tail -1)$2.bin
+    
+    
+    # move into the LUXSim directory, set G4 env, and run the macro
+    cd $LUXSIM_DIR
+    source $G4_DIR/etc/geant4env.sh $G4VER
+    $LUXSIM_DIR/LUXSimExecutable $OUTPUT_DIR/$MACRO_FILE
+    
+    # after macro has run, rootify
+    cd $OUTPUT_DIR
+    source $ROOT_DIR/bin/thisroot.sh
+    $LUXSIM_DIR/tools/LUXRootReader $OUTPUT_FILE
+    OUTPUT_ROOT_FILE=$(basename $OUTPUT_FILE .bin).root
+    
+    # reduce and then copy both to our central storage.
+    
+    source $LIBNEST_DIR/release-$LIBNEST_VERSION/libNEST/thislibNEST.sh
+    OUTPUT_REDUCED_FILE=${{OUTPUT_ROOT_FILE/".root"/"_analysis_tree.root"}}
+    $CONVERTER_EXE $OUTPUT_DIR/$OUTPUT_ROOT_FILE $OUTPUT_DIR/$OUTPUT_REDUCED_FILE
+    
+    dirac-dms-add-file $BIGROOT_STORE_PATH/$OUTPUT_ROOT_FILE $OUTPUT_DIR/$OUTPUT_ROOT_FILE $DATA_STORE_SE
+    dirac-dms-add-file $REDUCED_STORE_PATH/$OUTPUT_REDUCED_FILE $OUTPUT_DIR/$OUTPUT_REDUCED_FILE $DATA_STORE_SE
+    """).lstrip()
 
     def configure(self, master_appconfig):
-        input_workspace = self.getJobObject().getInputWorkspace().getPath()
-        run_script = os.path.join(input_workspace, 'runLUXSim_parametric.sh')
-        with open(run_script, 'wb') as f:
-            f.write(self.run_script())
-        return (None, StandardJobConfig(exe=run_script))
+        run_script = FileBuffer('runLUXSim_parametric.sh',
+                                LZApp.runscript.format(luxsim_version=self.luxsim_version,
+                                                       g4_version=self.g4_version,
+                                                       libnest_version=self.libnest_version,
+                                                       reduction_version=self.reduction_version),
+                                executable=True)
+        
+        with open(self.macro, 'rb') as original_macro:
+            macro = FileBuffer(os.path.basename(self.macro), original_macro)
+            macro += dedent("""
+            /control/getEnv SEED
+            /LUXSim/randomSeed {SEED}
+            /LUXSim/beamOn %s
+            exit
+            """ % self.nevents)
+
+        return (None, StandardJobConfig(exe=run_script, inputbox=[macro]))
 
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\#
 
@@ -98,7 +105,6 @@ class LZApp(IApplication):
 from Ganga.GPIDev.Adapters.ApplicationRuntimeHandlers import allHandlers
 from GangaLZ.Lib.RTHandlers.LZDiracRunTimeHandler import LZDiracRunTimeHandler
 
-allHandlers.add('LZApp', 'Local', LZDiracRunTimeHandler)
 allHandlers.add('LZApp', 'Dirac', LZDiracRunTimeHandler)
 
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\#
--- python/GangaLZ/Lib/RTHandlers/LZDiracRunTimeHandler.py
+++ python/GangaLZ/Lib/RTHandlers/LZDiracRunTimeHandler.py
@@ -1,11 +1,10 @@
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\#
 import os
-from GangaGaudi.Lib.RTHandlers.RunTimeHandlerUtils import script_generator
-from GangaDirac.Lib.RTHandlers.DiracRTHUtils import mangle_job_name, diracAPI_script_template, diracAPI_script_settings
+from textwrap import dedent
+
 from Ganga.GPIDev.Adapters.IRuntimeHandler import IRuntimeHandler
 from Ganga.GPIDev.Adapters.StandardJobConfig import StandardJobConfig
-from Ganga.Utility.Config import getConfig
-from Ganga.Utility.util import unique
+from Ganga.GPIDev.Lib.File.FileBuffer import FileBuffer
 #\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\#
 
 
@@ -13,22 +12,40 @@ class LZDiracRunTimeHandler(IRuntimeHandler):
 
     """The runtime handler to run Gaudi jobs on the Dirac backend"""
 
+    dirac_script_template = dedent(r"""
+    # dirac job created by ganga
+    from DIRAC.Core.Base.Script import parseCommandLine
+    parseCommandLine()
+    from DIRAC.Interfaces.API.Dirac import Dirac
+    from DIRAC.Interfaces.API.Job import Job
+    
+    dirac = Dirac()
+    j = Job()
+    
+    j.setName({name!r})
+    j.setExecutable({executable!r}, {arguments!r}, {logfile!r})
+    j.setInputSandbox({inputsandbox!s})
+    j.setOutputSandbox({outputsandbox!s})
+    j.setGenericParametricInput({parameters!s})
+    #j.setParameterSequence("arguments", {parameters!s}, addToWorkflow=True):
+    
+    # submit the job to dirac
+    j.setPlatform( 'ANY' )
+    result = dirac.submit(j)
+    output(result)
+    """).lstrip()
+
     def prepare(self, app, appsubconfig, appmasterconfig, jobmasterconfig):
 
-        dirac_script = script_generator(diracAPI_script_template(),
-                                        DIRAC_IMPORT='from DIRAC.Interfaces.API.Dirac import Dirac',
-                                        DIRAC_JOB_IMPORT='from DIRAC.Interfaces.API.Job import Job',
-                                        DIRAC_OBJECT='Dirac()',
-                                        JOB_OBJECT='Job()',
-                                        NAME=mangle_job_name(app),
-                                        EXE=appsubconfig.exe,
-                                        EXE_LOG_FILE='Ganga_output.log',
-                                        OUTPUT_SANDBOX=appsubconfig.outputbox,
-                                        OUTPUT_SE=getConfig(
-                                            'DIRAC')['DiracOutputDataSE'],
-                                        INPUT_SANDBOX=appsubconfig.inputbox
-                                        )
+        macro_name = os.path.basename(app.macro)
+        jobname = os.path.splitext(macro_name)[0] + "%s"
+        arguments = macro_name + " %s"
+        dirac_script = LZDiracRunTimeHandler.dirac_script_template.format(name=jobname,
+                                                                          executable=appsubconfig.exe.name,
+                                                                          arguments=arguments,
+                                                                          logfile="LUXSIM_output.log",
+                                                                          inputsandbox=[i.name for i in appsubconfig.inputbox],
+                                                                          outputsandbox=[],
+                                                                          parameters=range(app.seed, app.seed + app.njobs))
 
-        return StandardJobConfig(dirac_script,
-                                 inputbox=appsubconfig.inputbox,
-                                 outputbox=appsubconfig.outputbox)
+        return StandardJobConfig(dirac_script, inputbox=appsubconfig.getSandboxFiles(), outputbox=[])
